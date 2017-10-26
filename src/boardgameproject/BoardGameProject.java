1805238a4e98bd95f192cdfff9fc4e4da733b9e3
NoahHalbig <33099291+NoahHalbig@users.noreply.github.com>
2017-10-26 09:39:08 -0700

    Update BoardGameProject.java

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -1,10 +1,10 @@
-package boardgameproject;
+Mason is a package boardgameproject;
 
 public class BoardGameProject {
    
     public static void main(String[] args) {
         
-       
+       We are doing settlers of Catan
        
        
     }
