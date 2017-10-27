a5036e3070e1e34e632a93be89434ad948f64384
mas1346 <33130756+mas1346@users.noreply.github.com>
2017-10-27 09:28:43 -0700

    Update BoardGameProject.java

--- src/boardgameproject/BoardGameProject.java
+++ src/boardgameproject/BoardGameProject.java
@@ -3,10 +3,8 @@ package boardgameproject;
 public class BoardGameProject {
    
     public static void main(String[] args) {
-        
-       
-       
-       
-    }
+    int noah = 0;    
+    if(noah == 0)  
+       System.out.println("Noah");
     
 }
